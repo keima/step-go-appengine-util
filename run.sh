4b7a6f23191d23e9759956753c73367e71859e45
Kouta Imanaka <keima@p-side.net>
2016-03-02 20:58:43 +0900

    外部ファイルに切り出したらうまく動かなかったのでひとつに戻す

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-. `dirname $0`/util.sh
-
 readonly GAE_VERSION_LOG_FILE="$WERCKER_CACHE_DIR/go_appengine_version"
 readonly GAE_SDK_PATH="$WERCKER_CACHE_DIR/go_appengine"
 readonly GAE_LOG_TIMESTAMP=1
@@ -13,6 +11,39 @@ readonly UNZIPPER=7z # unzip
 readonly UNZIPPER_OPTION="x" # "-q -o"
 readonly UNZIPPER_PKG_APT=p7zip-full
 readonly UNZIPPER_PKG_YUM=p7zip
+
+# get string at file's spefified line number
+# singleline FILE LINE_NUM
+#   singleline hoge 5 -> (line 5 at hoge) and return (head and tail return val)
+#   singleline hoge five -> "" and return 1
+#   singleline NOT_EXIST 5 -> "" and return 1
+#   singleline hoge 99999(not exist line) -> "" and return 0
+singleline() {
+    # argument check
+    expr "$2" + 1 >/dev/null 2>&1
+    if [ $? -ge 2 ]; then
+        echo ""; return 1
+    fi
+
+    if [ ! -e $1 ]; then
+        echo ""; return 1
+    fi
+
+    # logic
+    head -$2 $1 | tail -1
+}
+
+# check semver
+#   semverlte 1.2.3 1.2.4 -> true
+#   semverlte 1.2.3 1.2.3 -> true
+#   semverlte 1.2.4 1.2.3 -> false
+# @see http://stackoverflow.com/a/4024263
+semverlte() {
+    [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+##########
+
 
 setup_gopath() {
     if [ ! -d $GAE_GOPATH ]; then
@@ -124,11 +155,10 @@ do_upgrade() {
     cd $WERCKER_CACHE_DIR
     local CURRENT_TIME=$(date +"%s")
     echo -e "$CURRENT_TIME\n$LATEST" > $GAE_VERSION_LOG_FILE
+
+    ls -al # debug
+
     cd -
-
-    # debug
-    ls -al
-
   else
     fail "\$LATEST is empty"
   fi
